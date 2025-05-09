9526df3a3df1c9b87f1a6ead5ff7ff7c59caf1e7
lordeber15 <eberpalomino@hotmail.com>
2025-05-09 09:08:50 -0500

    cambios variados

--- src/database/database.js
+++ src/database/database.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize(
     },
   }*/
 
-  "postgres://postgres:123456@localhost:5432/ordenesdeservicio"
+  "postgres://postgres:123456@localhost:5433/ordenesdeservicio"
 );
 
 module.exports = sequelize;
