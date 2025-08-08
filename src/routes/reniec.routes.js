20cf85d1c20e533d72666afe15abe997e4a0cb69
Eberson Palomino <eberpalomino@hotmail.com>
2025-08-08 17:42:42 -0500

    Update reniec.routes.js

--- src/routes/reniec.routes.js
+++ src/routes/reniec.routes.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 const { getReniec } = require("../controllers/reniec.controller.js");
 const router = Router();
 
-router.get("/api/reniec/:dni", getReniec);
+router.get("/v1/reniec/:dni", getReniec);
 
 module.exports = router;
